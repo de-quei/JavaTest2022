b005db62669d6693871ba151ba008c445f49299a
Hyunji Kim <papertoy01@gmail.com>
2023-03-07 08:53:46 +0900

    remove variable

--- CodingTest/src/Programmers/evenodd_num.java
+++ CodingTest/src/Programmers/evenodd_num.java
@@ -9,19 +9,17 @@ public class evenodd_num {
 		// TODO Auto-generated method stub
 		int[] num_list = new int[] {1,2,3,6,7};
 		int[] odd_even = new int[2];
-		int even_cnt = 0;
-		int odd_cnt = 0;
 		
 		for(int i = 0; i < num_list.length; i++) {
 			if(num_list[i]%2 == 0) {
-				even_cnt++;
+				odd_even[0]++;
 			}
 			else {
-				odd_cnt++;
+				odd_even[1]++;
 			}
 		}
 		
-		System.out.println("짝수 : " + even_cnt + ", 홀수 : " + odd_cnt);
+		System.out.println("짝수 : " + odd_even[0] + ", 홀수 : " + odd_even[1]);
 	}
 
 }
