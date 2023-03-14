76146a03b9a6bbb495477cc786e71e6d03082a3a
Hyunji Kim <papertoy01@gmail.com>
2023-03-14 16:33:57 +0900

    replace

---
+++ CodingTest/src/School_CodingTest/Replace_Test.java
@@ -0,0 +1,14 @@
+package School_CodingTest;
+
+public class Replace_Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String str = "I have a pen. I have an apple. I have pineapple";
+
+        str = str.replace("I", "You"); // I 문자를 YOU로 변경
+
+        System.out.println(str);
+	}
+
+}
